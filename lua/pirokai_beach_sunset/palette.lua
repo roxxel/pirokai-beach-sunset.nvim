191ae23274acf80211349afded23dc786f21f78a
roxxel <64275777+roxxel@users.noreply.github.com>
2026-09-13 23:31:36 +0300

    feat: add Pirokai palette

---
+++ lua/pirokai_beach_sunset/palette.lua
@@ -0,0 +1,24 @@
+return {
+  bg = "#131313",
+  bg_dark = "#0c0c0c",
+  bg_alt = "#191919",
+  bg_float = "#1b1b1b",
+  surface = "#222222",
+  surface_alt = "#363537",
+  border = "#2b2b2b",
+  muted = "#525053",
+  subtle = "#86838A",
+  comment = "#8b888f",
+  fg_dim = "#bab6c0",
+  fg = "#f7f1ff",
+
+  red = "#fc618d",
+  orange = "#fd9353",
+  parameter = "#f29a8a",
+  yellow = "#fce566",
+  green = "#7bd88f",
+  cyan = "#5ad4e6",
+  purple = "#948ae3",
+
+  none = "NONE",
+}
